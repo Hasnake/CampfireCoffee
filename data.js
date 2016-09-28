5f22b674e1c46260f7a3dc595c3691521c0aaa30
Hasnake <haileyesus46@gmail.com>
2016-09-28 02:25:00 -0700

    rounding decimals to one digit and customer number to whole number

--- data.js
+++ data.js
@@ -1,31 +1,31 @@
 'use strict';
 var store = {
   name:'Pike Place Market',
-  minCustomersPerHour : null,
-  maxCustomersPerHour:null,
-  averageCupsPerCust:null,
-  averagetogoPoundsPerCust:null,
+  minCustomersPerHour : 14,
+  maxCustomersPerHour:35,
+  averageCupsPerCust:1.2,
+  averagetogoPoundsPerCust:0.34,
 
 
   numberOfCustomers: function (){
-    return Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour) + (this.minCustomersPerHour);},
+    return Math.floor((Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour) + (this.minCustomersPerHour)));},
 
   numberOfCupsSoldPerHour :function() {
-    this.averageCupsPerCust * this.numberOfCustomers;},
+    return Math.round(this.averageCupsPerCust * this.numberOfCustomers() * 10) / 10;},
 
   numberOfPoundsSoldPerHour :function(){
-    this.averagetogoPoundsPerCust * this.numberOfCustomers;},
+    return Math.round(this.averagetogoPoundsPerCust * this.numberOfCustomers() * 10) / 10;},
   amountOfBeansInPoundsNeededForCups :function(){
-    this.numberOfCupsSoldPerHour / 16;},
+    return Math.round(this.numberOfCupsSoldPerHour() / 16 * 10) / 10;},
   numberOfEmployees: function(){
-    (this.numberOfCustomers / 30);},
+    return Math.round((this.numberOfCustomers() / 30) * 10) / 10;},
   totalAmountOfBeansInPoundsSoldPerHour:function(){
-    (this.amountOfBeansInPoundsNeededForCups) + (this.numberOfPoundsSoldPerHour);
+    return Math.round((this.amountOfBeansInPoundsNeededForCups()) + (this.numberOfPoundsSoldPerHour()) * 10) / 10;
   }
 };
 
-console.log(store.numberOfCustomers(35,14));
-console.log(store.numberOfCupsSoldPerHour(1.2));
-console.log(store.numberOfPoundsSoldPerHour(0.34));
-console.log(store.amountOfBeansInPoundsNeededForCups());
-console.log(store.numberOfEmployees());
+console.log(store.totalAmountOfBeansInPoundsSoldPerHour() + 'lbs[',
+store.numberOfCustomers() + ' customers,' ,
+store.numberOfCupsSoldPerHour() + ' cups(' +
+store.numberOfPoundsSoldPerHour() + ' lbs),',
+store.amountOfBeansInPoundsNeededForCups() + ' lbs to-go]');
