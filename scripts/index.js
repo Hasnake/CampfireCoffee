d2a418362ccd7823ce4e78f5d347618ca0c88291
Hasnake <haileyesus46@gmail.com>
2016-10-06 19:10:45 -0700

    Fixing an issue on the internal page

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@
 // image1El.src = 'assets/campfire-coffee.jpg';
 // document.body.appendChild(image1El);
 var h1El = document.createElement('h1');
-h1El.textContent = 'Public facing Page';
+h1El.textContent = 'Public Facing Page';
 document.body.appendChild(h1El);
 var t = document.createTextNode('This is the public page for the camfire coffee project.');
 document.body.appendChild(t);
@@ -21,14 +21,14 @@ a.href = 'pages/data.html';
 document.body.appendChild(a);
 
 var storeLocations = ['Pike Place Market','Capitol Hill','Seattle Public Library','South Lake Union','SeaTac Airport'];
-var storeLocationsEl = document.getElementById('storeLocations');
+// var storeLocationsEl = document.getElementById('storeLocations');
 var ulEl = document.createElement('ul');
-ulEl.textContent = 'Store Locations' + ' ---' + '        Working Hour:6am to 9pm';
+ulEl.textContent = ('Store Locations' + ' ---' + '        Working Hour:6am to 9pm');
 document.body.appendChild(ulEl);
 for (var i = 0;i < storeLocations.length;i++){
   var liEl = document.createElement('li');
   liEl.textContent = storeLocations[i] ;
   ulEl.appendChild(liEl);
 }
-storeLocationsEl.appendChild(ulEl);
-document.body.appendChild(storeLocationsEl);
+// storeLocationsEl.appendChild(ulEl);
+// document.body.appendChild(storeLocationsEl);
